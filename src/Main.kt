c83f64843e9694c76819df94ae4409e1feac4e58
LUCAS ALEXANDRE DA SILVA <25203702@dsn127m1242313.cfp127aluno.local>
2026-09-03 16:55:45 -0300

    feat: inicialização do exercício 01 (parte 2) - Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,18 @@
 import java.sql.Array
 import kotlin.collections.toList
+import kotlin.time.measureTime
 
-fun main (){
+fun main () {
 
     // ---| LISTA DE EXERCÍCIOS |---
 
     // ---| EXERCÍCIO 01 |---
 
-    fun numberParImpar(number: Int) : Int{
+    fun numberParImpar(number: Int): Int {
 
         val result = number % 2
 
-        when(result){
+        when (result) {
 
             0 -> println(" O $number é um número PAR")
             1 -> println(" O $number é um número IMPAR")
@@ -20,28 +21,28 @@ fun main (){
         return result
     }
 
-    numberParImpar(2)
+    //numberParImpar(2)
 
 
     // ---| EXERCÍCIO 02 |---
 
-    fun tipoNumerico(number: Int) : String{
+    fun tipoNumerico(number: Int): String {
 
-       val status = if(number == 0){
-           "ZERO"
+        val status = if (number == 0) {
+            "ZERO"
 
-       }else if(number >= 0){
-           "POSITIVO"
+        } else if (number >= 0) {
+            "POSITIVO"
 
-       }else{
-           "NEGATIVO"
-       }
+        } else {
+            "NEGATIVO"
+        }
 
         val result = ("O número $number é $status")
         return result
     }
 
-    println(tipoNumerico(10))
+    //println(tipoNumerico(10))
 
     // ---| EXERCÍCIO 03 |---
 
@@ -50,63 +51,63 @@ fun main (){
 
         val mensagem = when (idade) {
 
-            in 11 downTo 0  -> "A sua idade é de $idade anos! Você é uma CRIANÇA"
-            in 12 .. 17     -> "A sua idade é de $idade anos! Você é um ADOLESCENTE"
-            in 18 until 60  -> "A sua idade é de $idade anos! Você é um ADULTO"
-            else                  -> "A sua idade é de $idade anos! Você é um IDOSO"
+            in 11 downTo 0 -> "A sua idade é de $idade anos! Você é uma CRIANÇA"
+            in 12..17 -> "A sua idade é de $idade anos! Você é um ADOLESCENTE"
+            in 18 until 60 -> "A sua idade é de $idade anos! Você é um ADULTO"
+            else -> "A sua idade é de $idade anos! Você é um IDOSO"
         }
 
         return mensagem
     }
 
 
-    println(faixaEtaria(60))
+    //println(faixaEtaria(60))
 
 
     // ---| EXERCÍCIO 04 |---
 
-    fun tabuada(number: Int){
+    fun tabuada(number: Int) {
 
-        for( i in 1 .. 10){
-            println(number * i)
+        for (i in 1..10) {
+            println("$number x $i = ${number * i}")
         }
     }
 
-    println(tabuada(4))
+    //println(tabuada(4))
 
 
     // ---| EXERCÍCIO 05 |---
 
-    fun somaArrays(array: List<Int>): Int{
+    fun somaArrays(array: List<Int>): Int {
 
         var result = array.sum()
         return result
     }
 
     val numeros = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(somaArrays(numeros))
+    //println(somaArrays(numeros))
 
 
     // ---| EXERCÍCIO 06 |---
 
-    fun maiorNumberArray(array: List<Int>): Int?{
+    fun maiorNumberArray(array: List<Int>): Int? {
 
         val result = array.maxOrNull()
         return result
     }
 
-    println(maiorNumberArray(numeros))
+    //println(maiorNumberArray(numeros))
 
     // ---| EXERCÍCIO 07 |---
 
-    fun contagemRegressiva(number: Int){
+    fun contagemRegressiva(number: Int) {
 
-        for( i in number  downTo  0){
+        for (i in number downTo 0) {
             println(i)
         }
     }
 
-    contagemRegressiva(3)
+    //contagemRegressiva(3)
 
     // ---| EXERCÍCIO 08 |---
 
@@ -124,39 +125,112 @@ fun main (){
         return soma
     }
 
-    println(somarParesComFor(numeros))
+    //println(somarParesComFor(numeros))
 
     // ---| EXERCÍCIO 09 |---
 
-    fun dobroNumerico(number: Int): Int{
+    fun dobroNumerico(number: Int): Int {
 
-        val result = ( number * 2 )
+        val result = number * 2
         return result
     }
 
-    println(dobroNumerico(10))
+    //println(dobroNumerico(10))
 
 
     // ---| EXERCÍCIO 10 |---
 
-    fun mediaEscolar(nota1: Int, nota2: Int, nota3: Int): Int{
+    fun mediaEscolar(nota1: Int, nota2: Int, nota3: Int): Int {
 
         val somaNotas = nota1 + nota2 + nota3
-        val result    = somaNotas / 3
+        val result = somaNotas / 3
         return result
     }
 
-    println(mediaEscolar(5, 5, 5))
+    //println(mediaEscolar(5, 5, 5))
 
 
     // ---| EXERCÍCIO 11 |---
 
-    fun conversorTemperatura(celsius: Double): Double{
+    fun conversorTemperatura(celsius: Double): Double {
 
         val fahrenheit = (celsius * 1.8) + 32
         return fahrenheit
     }
 
-    println(conversorTemperatura(3.5))
+    //println(conversorTemperatura(3.5))
+
+
+    // ---| EXERCÍCIO 12 |---
+
+    fun batataQuente(): String{
+
+        val numbers = (100 .. 1).toList()
+        var status = ""
+
+        for (i in numbers) {
+
+            if (i % 3 == 0) {
+                status = "BATATA = $i"
+
+            } else if (i % 5 == 0) {
+                 status = "QUENTE = $i"
+
+            }else{
+                status = "BATATA-QUENTE = $i"
+            }
+        }
+
+        return status
+    }
+
+    // batataQuente()
+
+
+
+    // ---| EXERCÍCIO 13 |---
+
+    fun listaImpar(){
+
+        val listNumber = (100 ..1).toList()
+        val impar = listNumber.map { number -> number % 2 == 1}
+
+        print(impar)
+    }
+
+    // listaImpar()
+
+
+    // ---| EXERCÍCIO 14 |---
+
+    fun totalCompra(): Int{
+
+        val listPrecos = (100 .. 1).toList()
+        val result = listPrecos.sum()
+
+        return result
+    }
+
+    // totalCompra()
+
+
+    // ---| EXERCÍCIO 15 |---
+
+    fun mediaIdades(): Int{
+
+        val listaIdades = (100 .. 1).toList()
+        listaIdades.map { idade -> idade >= 18}
+
+        val quantidade   = listaIdades.size
+        val somaMaioral = listaIdades.sum()
+
+        val result = somaMaioral / quantidade
+        return result
+    }
+
+    // mediaIdades()
+
+    // ---| EXERCÍCIO 16 |---
+
 }
 
